d7ae1f9ef96f41301aac70c5510650b15e255941
umudoff <u.amrah@gmail.com>
2020-11-13 03:34:59 +0400

    Add project files.

---
+++ FileSystemVisitorProject/FileSystemVisitor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemVisitorProject
+{
+    public delegate void StartDelegate();
+    public delegate void FinishDelegate();
+    public class FileSystemVisitor
+    {
+        public event StartDelegate StartEvent;
+        public event FinishDelegate FinishEvent;
+
+        public IEnumerable ListDirectories(string path)
+        {
+            StartEvent();
+
+            var listDirs = new List<string>(Directory.GetDirectories(path));
+
+            foreach (var file in listDirs)
+            {
+                yield return file;
+            }
+
+            FinishEvent();
+        }
+
+    }
+}
---
+++ FileSystemVisitorProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemVisitorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+             FileSystemVisitor fileVisitor = new FileSystemVisitor();
+             fileVisitor.StartEvent += () => {
+                 Console.WriteLine(" FileVisitor started");
+             };
+            fileVisitor.FinishEvent += () =>
+            {
+                Console.WriteLine("FileVisitor finished");
+            };
+
+           foreach(var file in fileVisitor.ListDirectories(@"C:\Users\Amrah\Downloads\task2_advanced_c#\02. Advanced CSharp"))
+            {
+                Console.WriteLine(file);
+            }
+              
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
